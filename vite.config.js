16b8acd2272134dedb9986a8ea3b786eef91e0c5
ydvmudit07 <ydvmudit07@gmail.com>
2023-10-31 21:30:54 +0530

    minor bug fixed #4 (build)

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      external: ['arrow.svg', 'wbgMoo.png'],
+    },
+  },
 })
