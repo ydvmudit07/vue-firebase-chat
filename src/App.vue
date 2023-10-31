7278bc9c5749ffacd563e8a2375301a2669b2597
ydvmudit07 <ydvmudit07@gmail.com>
2023-10-31 21:03:10 +0530

    setting up the ui and functionalities of chatpage

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
     import db from './db';
     
     import DashboardHome from './components/DashboardHome.vue'
+    import DashboardChat from './components/DashboardCHat.vue'
     import HomeBox from './components/HomeBox.vue'
+
     const usernameInput = ref("")
     const messageInput = ref("")
     const state = reactive({
@@ -103,38 +105,50 @@
     <!-- CHAT PAGE STARTS FROM HERE -->
 
 
-    <div v-else>
-        <header>
-          <!-- <DashboardChat/> -->
-          <p>MooChat</p>
-          <p>Hello, {{ state.username }}</p>
-          <button @click="logout">Logout</button>
-        </header><br>
+    <div v-else class="bg-gray-900 h-[10vh]">
 
-        <section>
-          <div v-for="message in state.messages" :key="message.id" :class="(message.username == state.username ? 'message current-user' : 'message')">
-            <div>{{ message.username }}</div>
-            <div>{{ message.content }}</div>
-          </div>
-        </section>
+      <DashboardChat />
+      <header class="bg-gray-900 py-4 px-6 flex items-center justify-between pb-10">
+          <div class="text-white text-center sm:text-left flex-grow flex flex-col">
+          <!-- <p class="text-center items-center sm:text-left flex-grow-1">MooChat</p> -->
+          <p class="flex flex-col"><span class="text-lg">Welcome,</span><span class="text-5xl italic">{{ state.username }}</span></p>
+        </div>
+          <button @click="logout" class="ml-auto rounded-md bg-red-800 sm:px-4 py-1 text-base sm:text-lg font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Logout</button>
+      </header>
 
-        <footer>
-          <form @submit.prevent="sendMessage">
-            <input type="text" v-model="messageInput"/>
-            <input type="submit" value="Send Message"/>
-          </form>
-        </footer>
+
+        <div class="bg-gray-900 h-[71.7vh]">
+          <section class="bg-gray-700 chat-box">
+            <div v-for="message in state.messages" :key="message.id" :class="(message.username == state.username ? 'message current-user' : 'message')">
+              <div class="message-inner">
+                <div class="username">{{ message.username }}</div>
+                <div class="content">{{ message.content }}</div>
+              </div>
+            </div>
+          </section>
+        
+
+          <footer class="bg-gray-800">
+            <form @submit.prevent="sendMessage">
+              <input type="text" placeholder="Write your moo-ssage..." v-model="messageInput"/>
+              <input type="submit" value="Send Message"/>
+            </form>
+          </footer>
+        </div>
+        <!-- <Modal/> -->
+        <Testimonial/>
+
     </div>
   </template>
 
   <style scoped>
   .bouncing-image {
-      transition: transform 0.2s ease;
-    }
+    transition: transform 0.2s ease;
+  }
 
-    .bouncing-image:hover {
-      animation: bounce 0.5s infinite;
-    }
+  .bouncing-image:hover {
+    animation: bounce 0.5s infinite;
+  }
 
     @keyframes bounce {
       0%, 10%, 20%, 50%, 65%, 80%, 90%, 100% {
@@ -147,4 +161,137 @@
         transform: translateY(-5px);
       }
     }
+
+  .chat-box {
+    border-radius: 24px 24px 0 0;
+    /* background-color: #FFF; */
+    box-shadow: 0px 0px 10px rgba(100, 100, 100, 0.2);
+    flex: 1 1 100%;
+    padding: 30px;
+    width: 80vw;
+    height: 50vh;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+
+  /* Add scrollbar styling */
+  .chat-box::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  .chat-box::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  .chat-box::-webkit-scrollbar-thumb {
+    background-color: #888;
+    border-radius: 5px;
+  }
+
+  /* Handle scrollbar on hover */
+  .chat-box::-webkit-scrollbar-thumb:hover {
+    background-color: white;
+  }
+
+  .chat-box .message {
+    display: flex;
+    margin-bottom: 15px;
+  }
+
+  .chat-box .message .message-inner .username {
+    color: #888;
+    font-size: 16px;
+    margin-bottom: 5px;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+
+  .chat-box .message .message-inner .content {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #F3F3F3;
+    border-radius: 999px;
+    color: #333;
+    font-size: 18px;
+    line-height: 1.2em;
+    text-align: left;
+  }
+
+  .chat-box .message.current-user {
+    margin-top: 5px;
+    justify-content: flex-end;
+    text-align: right;
+  }
+
+  .chat-box .message.current-user .message-inner .content {
+    color: #FFF;
+    font-weight: 400;
+    background-color: rgb(31 41 55);
+  }
+
+  footer {
+    position: sticky;
+    bottom: 0px;
+    padding: 30px;
+    width: 80vw;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    border-radius: 0 0 24px 24px;
+    box-shadow: 0px 0px 10px rgba(100, 100, 100, 0.2);
+  }
+
+  footer form {
+    display: flex;
+  }
+
+  footer form input[type="text"] {
+    flex: 1 1 100%;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    display: block;
+    width: 100%;
+    height: 3rem;
+    padding: 10px 15px;
+    border-radius: 8px 0 0 8px;
+    color: #333;
+    font-size: 18px;
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+    background-color: #F3F3F3;
+    transition: 0.4s;
+  }
+
+  footer form input[type="text"]::placeholder {
+    color: #888;
+    transition: 0.4s;
+  }
+
+  footer form input[type="submit"] {
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    display: block;
+    border-radius: 0 8px 8px 0;
+    background-color: rgb(9, 53, 124);
+    border: 1px solid white;
+    color: #FFF;
+    font-weight: 500;
+
+    /* Increase padding for medium screens (sm and up) */
+    @media (min-width: 640px) {
+      padding: 10px 20px;
+      width: 10rem;
+    }
+  }
+
+  footer form input[type="submit"]:hover{
+    background-color: rgb(19, 80, 179);
+    cursor: pointer;
+  }
+
   </style>
--- src/components/DashboardChat.vue
+++ src/components/DashboardChat.vue
@@ -13,7 +13,7 @@
           <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
               <div class="flex-shrink-0">
-                <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                <img class="h-8 w-8" src="wbgMoo.png" alt="Moo" />
               </div>
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
@@ -23,7 +23,7 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-4 flex items-center md:ml-6">
-                <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <button type="button" class="relative rounded-full cursor-not-allowed bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span class="absolute -inset-1.5" />
                   <span class="sr-only">View notifications</span>
                   <BellIcon class="h-6 w-6" aria-hidden="true" />
@@ -35,7 +35,7 @@
                     <MenuButton class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span class="absolute -inset-1.5" />
                       <span class="sr-only">Open user menu</span>
-                      <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                      <img class="h-8 w-8 rounded-full" src="https://st4.depositphotos.com/3557671/23892/v/450/depositphotos_238923408-stock-illustration-vector-illustration-of-avatar-and.jpg" alt="" />
                     </MenuButton>
                   </div>
                   <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
@@ -67,7 +67,7 @@
           <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
               <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+                <img class="h-10 w-10 rounded-full" src="https://st4.depositphotos.com/3557671/23892/v/450/depositphotos_238923408-stock-illustration-vector-illustration-of-avatar-and.jpg" alt="" />
               </div>
               <div class="ml-3">
                 <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
@@ -85,40 +85,23 @@
           </div>
         </DisclosurePanel>
       </Disclosure>
-  
-      <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-        </div>
-      </header>
-      <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <!-- Your content -->
-        </div>
-      </main>
     </div>
   </template>
   
   <script setup>
   import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
   import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-  
-  const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  }
+
   const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
+    { name: 'Home', href: '#', current: false },
+    { name: 'Chat', href: '#', current: true },
+    { name: 'Github', href: 'https://github.com/ydvmudit07', current: false},
+    { name: 'Discord', href: 'https://discord.gg/Rga4253K', current: false},
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/ydvmudit07/', current: false},
   ]
   const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
+    { name: 'Welcome to MooChat', href: '#' },
+    // { name: 'Settings', href: '#' },
+    // { name: 'Sign out', href: '#' },
   ]
   </script>
